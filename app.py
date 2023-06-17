2745b8f3240ce1f5204c733c076ad09749568082
かわさきたくま <kawasakitakuma@kawasakitakumanoMacBook-Pro.local>
2023-06-17 15:28:11 +0900

    Data display elementsについて理解する

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 import streamlit as st
-st.title('Sample App')
-st.markdown('# 見出し1')
-st.markdown('## 見出し2')
-st.markdown('### 見出し3')
-
-
-st.markdown("""
-# 見出し1
-
-- 箇条書き1
-- 箇条書き2
-
-""")
-
-st.code('a = 123')
-
-st.code("""
-import numpy as np
 import pandas as pd
-a = 123
-pd.DataFrame()
-""")
+st.title('Sample App')
+
+df = pd.DataFrame({
+    '1列目': [1, 2, 3, 4],
+    '2列目': [-1, -2, -3, -4],
+})
+
+st.dataframe(df.style.highlight_max(axis=1))
+
+st.json({
+    'data':{
+        'name': 'abc',
+        'age': 123
+    }
+})
