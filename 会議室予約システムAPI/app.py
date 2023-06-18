6525e8e482f5da9d3fac7bed57e542480f770b46
かわさきたくま <kawasakitakuma@kawasakitakumanoMacBook-Pro.local>
2023-06-18 16:56:27 +0900

    テスト用roomの作成

--- 会議室予約システムAPI/app.py
+++ 会議室予約システムAPI/app.py
@@ -3,25 +3,52 @@ import random
 import requests
 import json
 
-st.title("API test user")
+page = st.sidebar.selectbox("Choose your page", ["users", "rooms"])
 
-with st.form(key="user"):
-    user_id:int = random.randint(0, 10)
-    username: str = st.text_input("ユーザー名", max_chars=12)
-    data = {
-        "user_id": user_id,
-        "user_name": username
-    }
-    submit_button = st.form_submit_button(label="リクエスト送信")
+if page == "users":
+    st.title("API test user")
+    with st.form(key="user"):
+        user_id:int = random.randint(0, 10)
+        username: str = st.text_input("ユーザー名", max_chars=12)
+        data = {
+            "user_id": user_id,
+            "user_name": username
+        }
+        submit_button = st.form_submit_button(label="リクエスト送信")
 
-if submit_button:
-    st.write("## 送信データ")
-    st.json(data)
-    st.write("## レスポンス結果")
-    url = "http://127.0.0.1:8000/users"
-    res = requests.post(
-        url,
-        data=json.dumps(data)
-    )
-    st.write(res.status_code)
-    st.json(res.json())
+    if submit_button:
+        st.write("## 送信データ")
+        st.json(data)
+        st.write("## レスポンス結果")
+        url = "http://127.0.0.1:8000/users"
+        res = requests.post(
+            url,
+            data=json.dumps(data)
+        )
+        st.write(res.status_code)
+        st.json(res.json())
+
+elif page == "rooms":
+    st.title("API test room")
+    with st.form(key="room"):
+        room_id:int = random.randint(0, 10)
+        room_name: str = st.text_input("会議室名", max_chars=12)
+        capacity: int = st.number_input("定員", step=1)
+        data = {
+            "room_id": room_id,
+            "room_name": room_name,
+            "capacity": capacity
+        }
+        submit_button = st.form_submit_button(label="リクエスト送信")
+
+    if submit_button:
+        st.write("## 送信データ")
+        st.json(data)
+        st.write("## レスポンス結果")
+        url = "http://127.0.0.1:8000/rooms"
+        res = requests.post(
+            url,
+            data=json.dumps(data)
+        )
+        st.write(res.status_code)
+        st.json(res.json())
