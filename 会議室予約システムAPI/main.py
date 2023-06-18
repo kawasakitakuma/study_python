db7ccfecfa2ff79f1eb7b19ac99ec5e247ffaf61
かわさきたくま <kawasakitakuma@kawasakitakumanoMacBook-Pro.local>
2023-06-18 16:08:38 +0900

    Bookingテーブルのモデル作成

--- 会議室予約システムAPI/main.py
+++ 会議室予約システムAPI/main.py
@@ -1,1 +1,11 @@
-print("a")
+import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+class Booking(BaseModel):
+    booking_id: int
+    user_id: int
+    room_id: int
+    booked_num: int
+    start_datetime: datetime.datetime
+    end_datetime: datetime.datetime
