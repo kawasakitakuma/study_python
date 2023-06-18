b0e9fc308011408249b669f3b127d900b6dbe929
かわさきたくま <kawasakitakuma@kawasakitakumanoMacBook-Pro.local>
2023-06-18 16:16:46 +0900

    試しのrootingを作成

--- 会議室予約システムAPI/main.py
+++ 会議室予約システムAPI/main.py
@@ -18,3 +18,21 @@ class Room(BaseModel):
     room_id: int
     room_name: str = Field(max_length=12)
     capacity: int
+
+app = FastAPI()
+
+@app.get("/")
+async def index():
+    return {"message" : "Success"}
+
+# @app.post("/users")
+# async def users(users: User):
+#     return {"users" : users}
+
+# @app.post("/rooms")
+# async def rooms(rooms: Room):
+#     return {"rooms" : rooms}
+
+# @app.post("/bookings")
+# async def bookings(bookings: Booking):
+#     return {"bookings" : bookings}
