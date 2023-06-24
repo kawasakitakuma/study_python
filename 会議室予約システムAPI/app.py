bb3f55f0aa5856564b7b1f557c9c57e93f1853ad
かわさきたくま <kawasakitakuma@kawasakitakumanoMacBook-Pro.local>
2023-06-24 21:37:31 +0900

    idを取得

--- 会議室予約システムAPI/app.py
+++ 会議室予約システムAPI/app.py
@@ -55,45 +55,69 @@ elif page == "rooms":
         st.json(res.json())
 
 elif page == "bookings":
-    st.title("API test bookings")
-    with st.form(key="booking"):
-        booking_id:int = random.randint(0, 10)
-        user_id:int = random.randint(0, 10)
-        room_id:int = random.randint(0, 10)
-        booked_num: int = st.number_input("予約人数", step=1)
-        date = st.date_input("日付:", min_value=datetime.date.today())
-        start_time = st.time_input("開始時刻: ", value=datetime.time(hour=9, minute=0))
-        end_time = st.time_input("終了時刻: ", value=datetime.time(hour=20, minute=0))
-        data = {
-            "booking_id": booking_id,
-            "user_id": user_id,
-            "room_id": room_id,
-            "booked_num": booked_num,
-            "start_datetime": datetime.datetime(
-                year=date.year,
-                month=date.month,
-                day=date.day,
-                hour=start_time.hour,
-                minute=start_time.minute
-            ).isoformat(),
-            "end_datetime": datetime.datetime(
-                year=date.year,
-                month=date.month,
-                day=date.day,
-                hour=end_time.hour,
-                minute=end_time.minute
-            ).isoformat()
-        }
-        submit_button = st.form_submit_button(label="リクエスト送信")
+    st.title("会議室予約画面")
+    # ユーザー一覧の取得
+    url_users = "http://127.0.0.1:8000/users"
+    res = requests.get(url_users)
+    users = res.json()
+    # ユーザー名をキー、ユーザーIDをバリュー
+    users_dict = {}
+    for user in users:
+        users_dict[user["username"]] =user["user_id"]
+    st.write(users_dict)
 
-    if submit_button:
-        st.write("## 送信データ")
-        st.json(data)
-        st.write("## レスポンス結果")
-        url = "http://127.0.0.1:8000/bookings"
-        res = requests.post(
-            url,
-            data=json.dumps(data)
-        )
-        st.write(res.status_code)
-        st.json(res.json())
+    # 会議室一覧の取得
+    url_users = "http://127.0.0.1:8000/rooms"
+    res = requests.get(url_users)
+    rooms = res.json()
+    # st.json(rooms)
+    rooms_dict = {}
+    for room in rooms:
+        rooms_dict[room["room_name"]] = {
+            "room_id": room["room_id"],
+            "capacity": room["capacity"]
+        }
+    st.write(rooms_dict)
+
+
+    # with st.form(key="booking"):
+    #     booking_id:int = random.randint(0, 10)
+    #     user_id:int = random.randint(0, 10)
+    #     room_id:int = random.randint(0, 10)
+    #     booked_num: int = st.number_input("予約人数", step=1)
+    #     date = st.date_input("日付:", min_value=datetime.date.today())
+    #     start_time = st.time_input("開始時刻: ", value=datetime.time(hour=9, minute=0))
+    #     end_time = st.time_input("終了時刻: ", value=datetime.time(hour=20, minute=0))
+    #     data = {
+    #         "booking_id": booking_id,
+    #         "user_id": user_id,
+    #         "room_id": room_id,
+    #         "booked_num": booked_num,
+    #         "start_datetime": datetime.datetime(
+    #             year=date.year,
+    #             month=date.month,
+    #             day=date.day,
+    #             hour=start_time.hour,
+    #             minute=start_time.minute
+    #         ).isoformat(),
+    #         "end_datetime": datetime.datetime(
+    #             year=date.year,
+    #             month=date.month,
+    #             day=date.day,
+    #             hour=end_time.hour,
+    #             minute=end_time.minute
+    #         ).isoformat()
+    #     }
+    #     submit_button = st.form_submit_button(label="リクエスト送信")
+
+    # if submit_button:
+    #     st.write("## 送信データ")
+    #     st.json(data)
+    #     st.write("## レスポンス結果")
+    #     url = "http://127.0.0.1:8000/bookings"
+    #     res = requests.post(
+    #         url,
+    #         data=json.dumps(data)
+    #     )
+    #     st.write(res.status_code)
+    #     st.json(res.json())
